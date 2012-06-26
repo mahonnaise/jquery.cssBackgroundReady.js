740f3f076b4ab6766fa2eed0039c91685ede0b36
Jos Hirth <github@kaioa.com>
2012-06-26 02:39:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#jquery.cssBackgroundReady.js
+
+*jquery.cssBackgroundReady.js* is a jQuery plugin which calls you back when the relevant background images are ready/preloaded.
+
+The background images of descendants are not included. Use [`$.add`](http://api.jquery.com/add/) if you want to include them.
+
+#Usage
+
+<code>$(<var>selector</var>).cssBackgroundReady(<var>handler</var>);</code>
+
+* `selector` &ndash; a jQuery [selector](http://api.jquery.com/category/selectors/)`
+* `handler` &ndash; a function to execute after all background images were loaded
+
+The `handler` callback is fired in the context of the jQuery collection. This means you can decide whether you want to perform one action, one action per element, or even both.
+
+```javascript
+$('.foo').cssBackgroundReady(function () {
+	console.log('this line is executed just once');
+	this.each(function () {
+		console.log('this line executed for every element');
+	});
+});
+```
---
+++ jquery.cssBackgroundReady.js
@@ -0,0 +1,54 @@
+/*jslint browser:true, regexp:false, plusplus:false*/
+/*global $*/
+
+(function ($) {
+	var urlRe = /^\s*url\("?([^"]*)"?\)$/;
+	$.fn.cssBackgroundReady = function (handler) {
+		var urls, collection, toLoad, loaded;
+
+		collection = this;
+
+		// get all URLs
+		urls = (function () {
+			var urlObj = {}, urls = [], url;
+			collection.each(function () {
+				var css = $(this).css('background-image'), parts, i;
+				parts = css.split(',');
+				for (i = parts.length; i--;) {
+					if (urlRe.test(parts[i])) {
+						urlObj[parts[i].match(urlRe)[1]] = true;
+					}
+				}
+			});
+			for (url in urlObj) {
+				if (urlObj.hasOwnProperty(url)) {
+					urls.push(url);
+				}
+			}
+			return urls;
+		}());
+
+		// invokes the callback once every image was loaded
+		loaded = function () {
+			toLoad--;
+			if (!toLoad && handler) {
+				//collection.each(handler);
+				handler.call(collection);
+			}
+		};
+
+		toLoad = urls.length;
+
+		// load all images
+		(function () {
+			var i, image;
+			for (i = toLoad; i--;) {
+				image = new Image();
+				image.onload = loaded;
+				image.src = urls[i];
+			}
+		}());
+
+		return collection;
+	};
+}(jQuery));
